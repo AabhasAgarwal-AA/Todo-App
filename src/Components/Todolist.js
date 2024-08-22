51f59afed873f6e15a887f32dffd6ac1c2af3613
AabhasAgarwal-AA <aabhasagarwal50@gamil.com>
2024-08-22 19:24:22 +0530

    todo app completed

---
+++ src/Components/Todolist.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Todo from './Todo';
+
+export const Todolist = ({todos, setTodos}) => {
+  return (
+    <div className='todo-ul'>
+      <ul>
+        {todos.map((todo)=>(
+          <Todo key={todo.id} id={todo.id} todo={todo} todos={todos} setTodos={setTodos} />
+        ))}
+
+      </ul>
+    </div>
+  )
+}
+
+export default Todolist;
